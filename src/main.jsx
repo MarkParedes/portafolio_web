a40bb4a48c33d3102071c07eaa4114d1c55b550f
Mark Brian Paredes Zavala <mark.brian.paredes.zavala@gmail.com>
2024-07-10 22:31:18 -0600

    Pruebas con React

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const Button = ({text}) => {
+  return (
+    <button>
+      {text}
+    </button>
+  )
+}
+
+root.render(
+  <React.Fragment>
+    <Button text="button 1" />
+    <Button text="button 2" />
+    <Button text="button 3" />
+  </React.Fragment>
 )
